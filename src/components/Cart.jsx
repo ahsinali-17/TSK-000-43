f19016037dc3536b9473ea3fa8ed8cce6c14e144
ahsinali-17 <aajoker17@gmail.com>
2024-06-10 13:17:44 +0500

    ...

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -10,7 +10,7 @@ const Cart = () => {
     <div className="products w-[90vw] m-auto flex flex-wrap justify-center gap-6 mt-6">
        {cart.map((item) => {
             return(
-            <div key={item.id} className='flex flex-col gap-5 items-center w-[45%] md:w-1/4 h-[35vh] lg:h-[50vh] border border-black rounded-xl p-6 cursor-pointer shadow-lg hover:shadow-gray-500'>
+            <div key={item.id} className='flex flex-col gap-3 items-center w-[45%] lg:w-1/4 h-[40vh] lg:h-[53vh] border border-black rounded-xl p-6 cursor-pointer shadow-lg hover:shadow-gray-500'>
                <img src={`${item.image}`} className='w-24 h-32'></img>
                 <h1 className='text-md font-semibold text-center h-[7vh]'>{item.title.slice(0,30)}</h1>
                 <span className='font-semibold text-lg'>{item.price} $</span>
